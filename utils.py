41db79ac3d2005b87f56f25bd598dd9f1316e10d
bogzak <bogdan.zaakh@gmail.com>
2025-02-08 14:42:44 +0300

    added annotations in utils.py

--- utils.py
+++ utils.py
@@ -3,24 +3,24 @@ import os
 from openai import OpenAI
 
 
-def read_file(file_path) -> str:
+def read_file(file_path: str) -> str:
     with open(file_path, "r", encoding="utf-8") as file:
         return file.read().strip()
     
 
-def read_file_lines(file_path) -> list[str]:
+def read_file_lines(file_path: str) -> list[str]:
     with open(file_path, "r", encoding="utf-8") as file:
         return [line.strip() for line in file if line.strip()]
     
 
-def get_file_size(file_path) -> int:
+def get_file_size(file_path: str) -> int:
     return os.path.getsize(file_path)
 
 
-def save_transcription_to_file(text, output_file_path):
+def save_transcription_to_file(text: str, output_file_path: str) -> None:
     with open(output_file_path, "w", encoding="utf-8") as file:
         file.write(text)
 
 
-def initialize_client(api_key) -> OpenAI:
+def initialize_client(api_key: str) -> OpenAI:
     return OpenAI(api_key=api_key)
